bf2dd6b205f94f03e83612f69082b2dad23c9f69
David13-117 <d.castillom55@gmail.com>
2026-06-02 21:45:45 -0500

    S. perfil #2

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -24,7 +24,7 @@ class UserLogin(BaseModel):
 
 class UserResponse(UserBase):
     id: Optional[str] = None
-    created_at: date
+    created_at: datetime
     is_active: bool = True
     birth_date: Optional[date] = None
     location: Optional[str] = None
